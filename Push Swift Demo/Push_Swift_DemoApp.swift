01dcb0204eff91581f2dcb78b357cc12e19a9516
Gbogboade Ayomide <ay4allcrown@gmail.com>
2024-06-11 15:18:18 +0100

    Initial Commit

---
+++ Push Swift Demo/Push_Swift_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Push_Swift_DemoApp.swift
+//  Push Swift Demo
+//
+//  Created by Ayomide Gbogboade on 11/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Push_Swift_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
